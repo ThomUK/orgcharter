b50dc82a92430af719131bb1e0844706e1ae0837
Tom Smith <thomas.smith5@nuh.nhs.uk>
2022-02-19 00:19:44 +0000

    fix global variable bindings

--- R/make_org_chart.R
+++ R/make_org_chart.R
@@ -1,39 +1,39 @@
 #' Make Org Chart
 #'
-#' @param org_data A dataframe prepared by prepare_org_data()
+#' @param .data A dataframe prepared by prepare_.data()
 #'
 #' @return A grViz object suitable for plotting
 #' @export
-make_org_chart <- function(org_data){
+make_org_chart <- function(.data){
 
   nodes <- data.frame(
-    people = unique(c(org_data$`Team Member`, org_data$Manager)) # de-duplicated list of all names
+    people = unique(c(.data$`Team Member`, .data$Manager)) # de-duplicated list of all names
     ) %>%
 
     # join labels in
-    dplyr::left_join(org_data %>% dplyr::select(`Team Member`, label) %>% unique(), by = c("people" = "Team Member")) %>%
+    dplyr::left_join(.data %>% dplyr::select(.data$`Team Member`, .data$label) %>% unique(), by = c("people" = "Team Member")) %>%
 
     dplyr::mutate(
       id = as.integer(dplyr::row_number()),
       type = NA, # required for certain graph types
       shape = "rectangle",
-      height = calc_box_height(label),
+      height = calc_box_height(.data$label),
       width = 1.5
     ) %>%
-    dplyr::filter(!is.na(people)) %>% #remove the node at the top of the tree (the recursive manager's manager)
-    dplyr::select(id, type, people, label, shape, height, width)
+    dplyr::filter(!is.na(.data$people)) %>% #remove the node at the top of the tree (the recursive manager's manager)
+    dplyr::select(.data$id, .data$type, .data$people, .data$label, .data$shape, .data$height, .data$width)
 
-  edges <- org_data %>%
+  edges <- .data %>%
     dplyr::left_join(nodes, by = c("Team Member" = "people")) %>% # join the node id's for people
-    dplyr::rename(from = id) %>%
+    dplyr::rename(from = .data$id) %>%
     dplyr::left_join(nodes, by = c("Manager" = "people")) %>%  # join the node id's for managers
-    dplyr::rename(to = id) %>%
+    dplyr::rename(to = .data$id) %>%
     dplyr::mutate(
       id = dplyr::row_number(),
       rel = NA # required for certain graph types
     ) %>%
-    dplyr::filter(!is.na(to)) %>% # remove the artificially added edges above top nodes
-    dplyr::select(id, from, to, rel, color, penwidth)
+    dplyr::filter(!is.na(.data$to)) %>% # remove the artificially added edges above top nodes
+    dplyr::select(.data$id, .data$from, .data$to, .data$rel, .data$color, .data$penwidth)
 
   g <- DiagrammeR::create_graph(
     nodes_df = nodes,
--- R/prepare_org_data.R
+++ R/prepare_org_data.R
@@ -22,14 +22,14 @@ prepare_org_data <- function(.data, include_job_titles = TRUE){
       )
     ) %>%
     dplyr::mutate(
-      `Team Member` = stringr::str_wrap(`Team Member`, 20),
-      Manager = stringr::str_wrap(Manager, 20),
-      `Job Title` = stringr::str_wrap(`Job Title`, 20),
+      `Team Member` = stringr::str_wrap(.data$`Team Member`, 20),
+      Manager = stringr::str_wrap(.data$Manager, 20),
+      `Job Title` = stringr::str_wrap(.data$`Job Title`, 20),
       label = dplyr::case_when(
         is.na(`Job Title`) | isFALSE(include_job_titles) ~ `Team Member`,
         TRUE ~ paste0(`Team Member`, "\n\n", `Job Title`)
       )
     ) %>%
-    dplyr::relocate(`Team Member`, Manager)
+    dplyr::relocate(.data$`Team Member`, .data$Manager)
 
 }
