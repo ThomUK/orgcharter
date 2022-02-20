9899e661369bf5b9cabf47f857a9a856fcb6701f
Tom Smith <thomas.smith5@nuh.nhs.uk>
2022-02-20 19:32:12 +0000

    add to main function

--- R/oc_make_org_chart.R
+++ R/oc_make_org_chart.R
@@ -8,11 +8,9 @@
 #' @export
 oc_make_org_chart <- function(.data, include_job_titles = TRUE){
 
-  org_data <- prepare_org_data(.data, include_job_titles)
+  nodes <- oc_nodes_dtf(.data, include_job_titles)
 
-  nodes <- oc_nodes_dtf(org_data)
-
-  edges <- oc_edges_dtf(org_data, nodes)
+  edges <- oc_edges_dtf(.data, nodes)
 
   g <- DiagrammeR::create_graph(
     nodes_df = nodes,
