67a26d1672628db9ba55f228dd4ac3dcf3f399d0
Tom Smith <thomas.smith5@nuh.nhs.uk>
2022-02-20 19:31:44 +0000

    dedicated function to create edges

--- R/oc_edges_dtf.R
+++ R/oc_edges_dtf.R
@@ -1,14 +1,27 @@
 oc_edges_dtf <- function(.data, nodes){
 
   edges <- .data %>%
-    dplyr::left_join(nodes, by = c("Team Member" = "people")) %>% # join the node id's for people
+    dplyr::left_join(nodes, by ="Team Member") %>% # join the node id's for people
     dplyr::rename(from = .data$id) %>%
-    dplyr::left_join(nodes, by = c("Manager" = "people")) %>%  # join the node id's for managers
+    dplyr::left_join(nodes, by = c("Manager" = "Team Member")) %>%  # join the node id's for managers
     dplyr::rename(to = .data$id) %>%
     dplyr::mutate(
       id = dplyr::row_number(),
       rel = NA # required for certain graph types
     ) %>%
+    dplyr::mutate(color = dplyr::case_when(
+      tolower(`Reporting Line`) == "solid" ~ "black",
+      tolower(`Reporting Line`) == "dotted" ~ "grey",
+      TRUE ~ "orange"
+    )) %>%
+    dplyr::mutate(
+      penwidth = dplyr::case_when(
+        `Reporting Line` == "Solid" ~ 2, # double width
+        `Reporting Line` == "Dotted" ~ 1, # single width
+        TRUE ~ 1
+      )
+    ) %>%
+
     dplyr::filter(!is.na(.data$to)) %>% # remove the artificially added edges above top nodes
     dplyr::select(.data$id, .data$from, .data$to, .data$rel, .data$color, .data$penwidth)
 
